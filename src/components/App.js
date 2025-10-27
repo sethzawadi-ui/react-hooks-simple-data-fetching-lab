3d73a280928461927b2873472eb51d2bc1fb6581
sethzawadi-ui <seth.zawadi@student.moringaschool.com>
2025-10-27 10:17:06 +0300

    react hooks simple data fetching

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+// create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [imageUrl, setImageUrl] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setImageUrl(data.message);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <img src={imageUrl} alt="A Random Dog" />
+      )}
+    </div>
+  );
+}
+
+export default App;
